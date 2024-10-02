ab6b161d527caa519cf79795c8fd2fd53e7979ba
Dominik <a00310099@AT-OA-82.tusstudent.ad.tus.ie>
2024-10-02 11:17:27 +0100

    Order class skeleton code

--- src/our_code/Order.java
+++ src/our_code/Order.java
@@ -1,5 +1,50 @@
 package our_code;
 
 public class Order {
-
+	private int orderID;
+	private Publication publication;
+	private String weekDay;
+	private int customerID;
+	
+	public Order() {
+		// TODO constructor
+	}
+	
+	// Getters
+	public int getID() {
+		return this.orderID;
+	}
+	public Publication getPublication() {
+		return this.publication;
+	}
+	public String getWeekDay() {
+		return this.weekDay;
+	}
+	public int getCustomerID() {
+		return this.customerID;
+	}
+	
+	// Setters
+	public void setPublication(Publication p) {
+		this.publication = p;
+	}
+	public void setWeekDay(String day) {
+		this.weekDay = day;
+	}
+	public void setCustomerID(int id) {
+		this.customerID = id;
+	}
+	
+	// Validate methods
+	public void validatePublication(Publication p) throws OrderException {
+		// TODO validate method
+	}
+	
+	public void validateWeekDay(String day) throws OrderException {
+		// TODO validate method
+	}
+	
+	public void validateCustomerID(int id) throws OrderException {
+		// TODO validate method
+	}
 }
---
+++ src/our_code/OrderException.java
@@ -0,0 +1,8 @@
+package our_code;
+
+public class OrderException extends Exception {	
+	public OrderException(String errorMessage) {
+		super(errorMessage);
+	}
+}
+
