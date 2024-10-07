227428f1660dfb59a9fb6d681b42ae82d7a11b87
A00308940 <A00308940@AT-V105-PC11.tusstudent.ad.tus.ie>
2024-10-07 15:43:09 +0100

    Docket Skeleton

--- src/our_code/DeliveryDocket.java
+++ src/our_code/DeliveryDocket.java
@@ -1,5 +1,50 @@
 package our_code;
 
 public class DeliveryDocket {
-
+	private int docketID;
+	private String theTime;// Possibly?
+	private String deliveryArea;
+	private boolean orderStatus;
+	private String listofOrders;
+	
+	public DeliveryDocket() {
+		
+	}
+	void setId(int dockID) {
+		docketID = dockID;
+	}
+	
+	public int getID()	{
+		return docketID;
+	}
+	
+	void setTime(String time)	{
+		theTime = time;
+	}
+	
+	public String getTime(){
+		return theTime;
+	}
+	void setArea(String delArea)
+	{
+		deliveryArea = delArea;
+	}
+	public String getArea()
+	{
+		return deliveryArea;
+	}
+	void setStatus(boolean status)
+	{
+		orderStatus = status;
+		
+	}
+	void setOrders(String orders)
+	{
+		listofOrders = orders;
+	}
+	public String getOrders()
+	{
+		return listofOrders;
+	}
+	
 }
