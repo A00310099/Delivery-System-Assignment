65ea37ae4ebeeffcd8016179cef79404415bf5b8
A00312257 <A00312257@AT-X207-PC18.tusstudent.ad.tus.ie>
2024-11-18 11:43:42 +0000

    Mohid

---
+++ src/our_code/DeliveryPersonExceptionHandler.java
@@ -0,0 +1,23 @@
+package our_code; 
+
+ 
+
+public class DeliveryPersonExceptionHandler extends Exception { 
+
+ 
+
+ 
+
+ 
+
+public DeliveryPersonExceptionHandler(String errorMessage){ 
+
+super(errorMessage); 
+
+} 
+
+ 
+
+ 
+
+} 
